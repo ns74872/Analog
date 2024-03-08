6da439f782e3c184bc7f34741fa17635558d56d0
ns74872 <130915228+ns74872@users.noreply.github.com>
2024-03-08 20:38:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+setInterval(()=> {
+d = new Date();
+h = d.getHours();
+m = d.getMinutes();
+s = d.getSeconds();
+hrotation = 30*h + m/2;
+mrotation = 6*m;
+srotation = 6*s;
+
+hour.style.transform = `rotate(${hrotation}deg)`;
+min.style.transform = `rotate(${mrotation})`;
+sec.style.transform = `rotate(${srotation}deg)`;
+
+
+},1000);
